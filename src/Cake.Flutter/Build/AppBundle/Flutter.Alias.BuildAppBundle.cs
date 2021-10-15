18f6b0d45ed67729f12f854354cf93ce77f8ac04
Miha Markič <miha@rthand.com>
2021-10-15 16:34:56 +0200

    #Closes 8 adds Build AppBundle command

---
+++ src/Cake.Flutter/Build/AppBundle/Flutter.Alias.BuildAppBundle.cs
@@ -0,0 +1,46 @@
+using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+using System.Collections.Generic;
+
+namespace Cake.Flutter
+{
+	partial class FlutterAliases
+	{
+         /// <summary>
+	    /// Build an Android app bundle file from your app.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="settings">The settings.</param>
+        
+		[CakeMethodAlias]
+		public static void FlutterBuildAppBundle(this ICakeContext context, FlutterBuildAppBundleSettings settings)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+            var runner = new GenericRunner<FlutterBuildAppBundleSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+			 runner.Run("build apk", settings ?? new FlutterBuildAppBundleSettings());
+		}
+
+
+        /// <summary>
+        /// Build an Android app bundle file from your app.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        /// <returns>Output lines.</returns>
+        [CakeMethodAlias]
+		public static IEnumerable<string> FlutterBuildAppBundleWithResult(this ICakeContext context, FlutterBuildAppBundleSettings settings)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+            var runner = new GenericRunner<FlutterBuildAppBundleSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+			return runner.RunWithResult("build apk", settings ?? new FlutterBuildAppBundleSettings());
+		}
+
+	}
+}
